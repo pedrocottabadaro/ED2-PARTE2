f528b4a41dc5af3c8a1dfe47503a2fdf14a28cb5
Pedro Cotta Badaro <37852363+pedrocottabadaro@users.noreply.github.com>
2019-11-30 19:52:55 -0300

    MAIN QUE CHAMA O MENU

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<iterator>
+#include <vector>
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include <fstream>
+#include "registro.h"
+#include "LZW.h"
+
+#include "Menu.h"
+using namespace std;
+
+
+
+
+int main(){
+
+   Menu m;
+   m.exibirMenu();
+
+
+}
